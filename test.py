618c0ea812e4f393305ee9790a386eb6291fcb7f
virus-rpi <72336443+virus-rpi@users.noreply.github.com>
2022-03-25 23:32:10 +0100

    virusrpi

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import sys
 
 from PyQt5.QtWidgets import *
+from PyQt5 import *
 
 from main_window_ui import Ui_MainWindow
 
@@ -14,11 +15,28 @@ class Window(QMainWindow, Ui_MainWindow):
         dialog = FindTest(self)
         dialog.exec()
 
-    def fill(self, list):
+    def fill(self, voclist):
+        self.label_1 = QLabel("x", self)
+        self.label_2 = QLabel("x", self)
+        self.label_3 = QLabel("x", self)
+        self.label_4 = QLabel("x", self)
+        self.label_5 = QLabel("x", self)
+        self.label_6 = QLabel("x", self)
+        self.label_7 = QLabel("x", self)
+        self.label_8 = QLabel("x", self)
+        self.label_9 = QLabel("x", self)
+        self.label_10 = QLabel("x", self)
+        self.label_11 = QLabel("x", self)
+        self.label_12 = QLabel("x", self)
+        self.label_13 = QLabel("x", self)
+        self.label_14 = QLabel("x", self)
+        self.label_15 = QLabel("x", self)
+        self.label_16 = QLabel("x", self)
+
         x = 1
-        for i in list:
+        for i in voclist:
             z = eval("self.label_" + str(x))
-            z = i
+            z.setText(i)
             x += 1
 
 
