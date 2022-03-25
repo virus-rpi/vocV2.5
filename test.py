6ca69520d85d50495eff801a031e7a93c8b809bb
virus-rpi <72336443+virus-rpi@users.noreply.github.com>
2022-03-25 23:49:48 +0100

    virusrpi

--- test.py
+++ test.py
@@ -16,23 +16,23 @@ class Window(QMainWindow, Ui_MainWindow):
         dialog.exec()
 
     def fill(self, voclist):
-        self.label_1 = QLabel("x", self)
-        self.label_2 = QLabel("x", self)
-        self.label_3 = QLabel("x", self)
-        self.label_4 = QLabel("x", self)
-        self.label_5 = QLabel("x", self)
-        self.label_6 = QLabel("x", self)
-        self.label_7 = QLabel("x", self)
-        self.label_8 = QLabel("x", self)
-        self.label_9 = QLabel("x", self)
-        self.label_10 = QLabel("x", self)
-        self.label_11 = QLabel("x", self)
-        self.label_12 = QLabel("x", self)
-        self.label_13 = QLabel("x", self)
-        self.label_14 = QLabel("x", self)
-        self.label_15 = QLabel("x", self)
-        self.label_16 = QLabel("x", self)
-
+        # self.label_1 = QLabel("x", self)
+        # self.label_2 = QLabel("x", self)
+        # self.label_3 = QLabel("x", self)
+        # self.label_4 = QLabel("x", self)
+        # self.label_5 = QLabel("x", self)
+        # self.label_6 = QLabel("x", self)
+        # self.label_7 = QLabel("x", self)
+        # self.label_8 = QLabel("x", self)
+        # self.label_9 = QLabel("x", self)
+        # self.label_10 = QLabel("x", self)
+        # self.label_11 = QLabel("x", self)
+        # self.label_12 = QLabel("x", self)
+        # self.label_13 = QLabel("x", self)
+        # self.label_14 = QLabel("x", self)
+        # self.label_15 = QLabel("x", self)
+        # self.label_16 = QLabel("x", self)
+#
         x = 1
         for i in voclist:
             z = eval("self.label_" + str(x))
@@ -47,7 +47,7 @@ class FindTest(QDialog):
 
 
 if __name__ == "__main__":
-    list = ["test ewgh", "test awogih"]
+    list = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10" "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"]
     app = QApplication(sys.argv)
     win = Window()
     win.fill(list)
